4be8b4ab71281d3426806666af49737778115a76
Aseke <askar.toleuovv@gmail.com>
2025-04-28 23:49:46 +0900

    bugfix v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
+      // ↳ turns “./src” relative to this config file into a FS path
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  }
 });
